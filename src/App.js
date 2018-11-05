6d924b58c90f8709989a77d6a101ccbe6d749993
Brayan Franco <Brayan.Franco@Focusservices.com>
2018-11-05 08:19:03 -0600

    finalizado

--- src/App.js
+++ src/App.js
@@ -6,46 +6,69 @@ import axios from 'axios';
 
 class App extends Component {
 
-  state ={
+  state = {
     monedas: [],
-    cotizacion:{},
-    monedaCotizada: ''
+    cotizacion: {},
+    monedaCotizada: '',
+    cargando: false
   }
 
-    async componentDidMount(){
-        this.obtenerMonedas();
-    }
-obtenerMonedas = async () =>{
-  const url = `https://api.coinmarketcap.com/v2/ticker/`;
+  async componentDidMount() {
+    this.obtenerMonedas();
+  }
+  obtenerMonedas = async () => {
+    const url = `https://api.coinmarketcap.com/v2/ticker/`;
 
-  await axios.get(url)
-  .then(respuesta =>{
-    this.setState({
-      monedas: respuesta.data.data
-    })
-  })
-  .catch(error =>{
-    console.log(error)
-  }) 
+    await axios.get(url)
+      .then(respuesta => {
+        this.setState({
+          monedas: respuesta.data.data
+        })
+      })
+      .catch(error => {
+        console.log(error)
+      })
 
-}
+  }
 
-obtenerValoresCrypto = async (monedas) =>{
-//console.log(monedas)
-const {moneda,crypto} = monedas;
-const url = `https://api.coinmarketcap.com/v2/ticker/${crypto}/?convert=${moneda}`;
-console.log(url);
+  obtenerValoresCrypto = async (monedas) => {
+    //console.log(monedas)
+    const { moneda, crypto } = monedas;
+    const url = `https://api.coinmarketcap.com/v2/ticker/${crypto}/?convert=${moneda}`;
+    console.log(url);
 
-await axios.get(url)
-.then(respuesta => {
-  this.setState({
-    cotizacion: respuesta.data.data,
-    monedaCotizada: moneda
-  })
-})
-}
+    await axios.get(url)
+      .then(respuesta => {
+        this.setState({
+          cargando: true
+        })
+        setTimeout(() => {
+          this.setState({
+            cotizacion: respuesta.data.data,
+            monedaCotizada: moneda,
+            cargando: false
+          })
+        }, 3000);
+      })
+  }
 
   render() {
+
+    const cargando = this.state.cargando;
+    let resultado;
+
+    if (cargando) {
+      resultado = <div className="spinner">
+                    <div className="double-bounce1"></div>
+                    <div className="double-bounce2"></div>
+                  </div>
+    } else {
+      resultado = <Resultado
+                     cotizacion={this.state.cotizacion}
+                     monedaCotizada={this.state.monedaCotizada}
+                  />
+    }
+
     return (
       <div className="container">
         <Header
@@ -53,15 +76,13 @@ await axios.get(url)
         />
 
         <div className="row justify-content-center">
-            <div className= "col-md-6 bg-light pb-4 contenido-principal">
-              <Form
-              monedas= {this.state.monedas}
-              obtenerValoresCrypto = {this.obtenerValoresCrypto}
-              />
-              <Resultado
-                cotizacion = {this.state.cotizacion}
-              />
-            </div>
+          <div className="col-md-6 bg-light pb-4 contenido-principal">
+            <Form
+              monedas={this.state.monedas}
+              obtenerValoresCrypto={this.obtenerValoresCrypto}
+            />
+              {resultado}
+          </div>
         </div>
       </div>
 
--- src/Components/Resultado.js
+++ src/Components/Resultado.js
@@ -3,14 +3,21 @@ import React, { Component } from 'react'
 export class Resultado extends Component {
 
     mostrarResultado = () => {
+        const monedaCotizada = this.props.monedaCotizada;
         const { name, quotes } = this.props.cotizacion;
         if (!name) return null
+
+        const {price, percent_change_1h, percent_change_24h} = quotes[monedaCotizada];
 
         return (
 
             <div className="bg-success py-4">
                 <div className="resumen text-light text-center">
                     <h2 className="mb-4">Resumen</h2>
+                    <p><span className = "font-weight-bold">El precio de {name} en {monedaCotizada} es de:</span> $ {(price).toFixed(2)}</p>
+                    <p><span className = "font-weight-bold">Porcentaje ultima hora:  </span>{percent_change_1h}%</p>
+                    <p><span className = "font-weight-bold">Porcentaje Ultimas 24h </span>{percent_change_24h}%</p>
+
                 </div>
 
             </div>
